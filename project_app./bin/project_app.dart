400c7d78749dd2715852e46b6e1b7ae715987821
ardilyce <ardilyuce@gmail.com>
2025-10-12 17:28:26 +0300

    Initial commit

---
+++ project_app./bin/project_app.dart
@@ -0,0 +1,5 @@
+import 'package:project_app/project_app.dart' as project_app;
+
+void main(List<String> arguments) {
+  print('Hello world: ${project_app.calculate()}!');
+}
---
+++ project_app./test/project_app_test.dart
@@ -0,0 +1,8 @@
+import 'package:project_app/project_app.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
