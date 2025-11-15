236a42f29f3c1cafcd7ff8914ca8ae153b651e84
ardilyce <ardilyuce@gmail.com>
2025-11-15 21:59:38 +0300

    changing folder structure for a flutter project

---
+++ old_console_project/project_app./lib/screens/splash_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+
+        // Background gradient
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Color(0xFF022047), // dark blue
+              Color(0xFF0A4DBA), // light blue
+            ],
+          ),
+        ),
+
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(height: 200),
+
+            // App Logo
+            Image.asset(
+              "assets/images/logo.png",
+              width: 130,
+              height: 130,
+            ),
+
+            const SizedBox(height: 30),
+
+            // Title
+            const Text(
+              "PhishGuard",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 38,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 1,
+              ),
+            ),
+
+            const SizedBox(height: 12),
+
+            // Subtitle
+            const Text(
+              "Detect scams before they catch you.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 18,
+              ),
+            ),
+
+            const Spacer(),
+
+            // Footer
+            const Padding(
+              padding: EdgeInsets.only(bottom: 40),
+              child: Text(
+                "CS310 Project – Sabancı University",
+                style: TextStyle(
+                  color: Colors.white54,
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
