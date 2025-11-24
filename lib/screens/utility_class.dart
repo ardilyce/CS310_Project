0331eb6768b3a31c647c62e5d3d2244a557d61c5
Arın İkizoğulları <139147071+arinikiz@users.noreply.github.com>
2025-11-24 12:07:03 +0300

    Update color constants and risk color values
    Updated s static risk colors , primaryBlue and SecondaryBlue so that they match precisely with the color pallets of the initial page design

--- lib/screens/utility_class.dart
+++ lib/screens/utility_class.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
 
 class AppUtility {
-  static const Color primaryBlue = Color(0xFF0A4DBA);
-  static const Color secondaryBlue = Color(0xFF022047);
+  static const Color primaryBlue = Color(0xFF0055AA);
+  static const Color secondaryBlue = Color(0xFFC5E2FF);
   static const Color textDark = Color(0xFF333333);
   static const Color textLight = Colors.white70;
   static const Color textLight2 = Colors.white54;
   static const Color background = Colors.white;
 
   // Dynamic Risk Colors
-  static const Color riskGreen = Color(0xFF72C936); // 0-25
-  static const Color riskYellow = Color(0xFFEBC334); // 25-50
-  static const Color riskOrange = Color(0xFFE66A34); // 50-75
-  static const Color riskRed = Color(0xFFA82222); // 75-100
+  static const Color riskGreen = Color(0xFF6BCD36); // 0-25
+  static const Color riskYellow = Color(0xFFDABC44); // 25-50
+  static const Color riskOrange = Color(0xFFD96938); // 50-75
+  static const Color riskRed = Color(0xFFA12325); // 75-100
 
   static Color getColorForScore(int score) {
     if (score <= 25) return riskGreen;
@@ -28,3 +28,4 @@ class AppUtility {
     return "high risk";
   }
 }
+
